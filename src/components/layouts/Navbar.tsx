55e5921a9dd0eb1c2fa0908613b2817bbbedf6f3
irorere juliet <irorerejuliet@gmail.com>
2026-01-27 13:03:20 +0100

    fixed error in navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaTimes } from "react-icons/fa";
 
